7e516eef6bfd0ef4a0491ee3fcd5e5c245bbdbdd
shiva676466 <vsgvarikuppala.com>
2026-04-02 21:44:32 +0530

    .

--- Recursion/binary_search.cpp
+++ Recursion/binary_search.cpp
@@ -1,11 +1,33 @@
+#include <algorithm>
 #include<iostream>
+#include<vector>
 using namespace std;
 
 // binary search always requires sorted array to perform operations
 
+int BS(vector<int> arr, int target, int st, int end){
+    if(st <= end){
+        int mid = st + (end - st) / 2;
 
+        if(arr[mid] == target)
+            return mid;
+        else if(arr[mid] < target)
+            return BS(arr, target, mid+1, end);
+        else
+            return BS(arr, target, st, mid-1);
+    }
+
+    return -1;
+}
+
+int search(vector<int> &arr, int target){
+    return BS(arr, target, 0, arr.size() - 1);
+}
 
 
 int main(){
+    vector<int> arr = {1, 2, 2, 3, 4,5, 6, 44};
+    int target = 4;
+    cout << search(arr, target);
     return 0;
 }
