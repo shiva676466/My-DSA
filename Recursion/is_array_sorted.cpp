5fef5aed4f1e36e498bf2cfc3cd22473bc292391
shiva676466 <vsgvarikuppala.com>
2026-04-02 21:18:04 +0530

    .

--- Recursion/is_array_sorted.cpp
+++ Recursion/is_array_sorted.cpp
@@ -6,12 +6,21 @@ bool isSorted(vector<int> arr, int n){
     if(n == 0 || n == 1){
         return true;
     }
-
     return arr[n-1] >= arr[n-2] && isSorted(arr, n-1);
 }
 
 
 int main(){
+
+    vector<int> arr1 = {1, 2, 3, 4};
+    cout << isSorted(arr1, arr1.size());
+    
+    cout << endl;
+    cout << endl;
+
+    vector<int> arr2 = {1, 2, 3, 4, 7, 0, -1};
+    cout << isSorted(arr2, arr2.size());
+
 
 
 
