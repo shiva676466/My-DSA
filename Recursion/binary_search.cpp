daf8cd6fde577f98a22778446accda7b08dce569
shiva676466 <vsgvarikuppala.com>
2026-04-02 21:50:35 +0530

    .

--- Recursion/binary_search.cpp
+++ Recursion/binary_search.cpp
@@ -11,15 +11,16 @@ int BS(vector<int> arr, int target, int st, int end){
         if(arr[mid] == target)
             return mid;
         else if(arr[mid] < target)
-            return BS(arr, target, mid+1, end);
+            return BS(arr, target, mid+1, end);     // if we know that target is at the second half then we can call the recursion for the second half only
         else
-            return BS(arr, target, st, mid-1);
+            return BS(arr, target, st, mid-1);     // if we know that target is at the first half then we can call the recursion for the first half only
     }
 
     return -1;
 }
 
 int search(vector<int> &arr, int target){
+    // this is basically a helper function: this acts as a wrapper around our recursive BS function, we have to give arr, target, st, end in main BS fn instead of giving all those params we create a helper fn that directly gives the result we need in only two params.
     return BS(arr, target, 0, arr.size() - 1);
 }
 
