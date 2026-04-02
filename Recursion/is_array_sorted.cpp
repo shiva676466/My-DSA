bcc63a332c93b041d20f97fae76022e0468750e3
shiva676466 <vsgvarikuppala.com>
2026-04-02 21:14:28 +0530

    .

--- Recursion/is_array_sorted.cpp
+++ Recursion/is_array_sorted.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 bool isSorted(vector<int> arr, int n){
-    
+    if(n == 0 || n == 1){
+        return true;
+    }
+
+    return arr[n-1] >= arr[n-2] && isSorted(arr, n-1);
 }
 
 
