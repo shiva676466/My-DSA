7cdb3935d76d23dedd4c58c384521d45eb66ed92
shiva676466 <vsgvarikuppala.com>
2026-04-03 19:56:51 +0530

    .

--- sample.cpp
+++ sample.cpp
@@ -1,1 +1,15 @@
-// this is a comment
+#include <algorithm>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int main(){
+
+    
+    vector<vector<int>> array;
+    array.push_back({1});
+    
+
+    return 0;
+}
