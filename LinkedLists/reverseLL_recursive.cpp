2a9eaef97bf5a905ae382f49f4da091930fcf482
shiva676466 <vsgvarikuppala.com>
2026-04-04 00:24:52 +0530

    added a file in ll folder

---
+++ LinkedLists/reverseLL_recursive.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+// common STL functions for linked lists
+// push_back()
+// push_front()
+// pop_back()
+// pop_front()
+// ll.front()
+// ll.back()
+// ll.size()
+
+list<int> Reverse_ll(list<int> head){
+
+}
+
+
+int main(void){
+    return 0;
+}
