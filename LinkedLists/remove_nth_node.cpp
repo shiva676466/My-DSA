25a6fedcab98ec9dcd2b126eb3466a848a46ad81
shiva676466 <vsgvarikuppala.com>
2026-04-04 16:02:23 +0530

    .

--- LinkedLists/remove_nth_node.cpp
+++ LinkedLists/remove_nth_node.cpp
@@ -12,4 +12,60 @@ struct Node {
     }
 };
 
+class Solution{
+public:
+    Node* deleteNode(Node* head, int k){
+        // handle edge cases for: empty list & for k = 1.
+        if(head == NULL){
+            return NULL;
+        }
+        if(k == 1){              // for k = 1
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return head;
+        }
+        Node *temp = head;
+        // move to the node just before the target node (k-1)th node for deleting the kth node.
+        for(int i = 1; i < k - 1 && temp->next != NULL; i++){
+            temp = temp->next;
+        }
 
+        if(temp->next != NULL){
+            Node* del = temp->next;
+            temp->next = temp->next->next;
+            delete del;
+        }
+
+        return head;
+    }
+};
+
+
+
+int main(void){
+
+    struct Node* ll;
+
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+
+    // creating obj
+    Solution obj;
+
+    int k = 3;
+
+    head = obj.deleteNode(head, k);
+
+    // printing the list after operation
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+
+    return 0;
+}
