a4f14325157dfe54881ecc42e31e29c722a69d91
shiva676466 <vsgvarikuppala.com>
2026-04-09 05:56:46 +0530

    updated

--- Recursion/reverse_a_str.cpp
+++ Recursion/reverse_a_str.cpp
@@ -1,11 +1,25 @@
 #include<iostream>
+#include<algorithm>
+#include <utility>
 using namespace std;
 
-string reverse_a_str(string name){
-    
+void reverse_a_str(string &str, int st, int end){
+    // base case
+    if(st >= end){
+        return ;
+    }
+    // swap
+    swap(str[st], str[end]);
+
+    // recursive case
+    reverse_a_str(str, st+1, end-1);
+    // this will makes "st" & "end" moves inward so that swapping happens easily
 }
 
 
 int main(void){
+    string s = "MOM";
+    reverse_a_str(s, 0, s.length()-1);
+    cout << s;
     return 0;
 }
