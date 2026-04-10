fb843f6de781b0b100322b95128f5a1318f2e13b
shiva676466 <vsgvarikuppala.com>
2026-04-10 19:08:34 +0530

    updated

--- SORTING/sample.cpp
+++ SORTING/sample.cpp
@@ -1,38 +1,31 @@
 #include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-// BUBBLE SORT
-void BubbleSort(int arr[], int n) {
+// ALL SORTING ALGORITHMS
 
-  bool swapped;
+// 1. bubble sort
+void bubbleSort(int arr[], int n) {
   for (int i = 0; i < n - 1; i++) {
-    swapped = false;
     for (int j = 0; j < n - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr[j], arr[j + 1]);
-        swapped = true;
       }
     }
-    if (!swapped) break;
   }
-
-  for (int val = 0; val < n; val++) {
-    cout << arr[val] << " ";
-  }
-  cout << endl;
 }
 
-void SelectionSort();
-void InsertionSort();
-void MergeSort();
-void QuickSort();
+int main(void) {
+  // for bubble sort
+  int arr[] = {3, 2, 1, 7, 6};
+  int n = 5;
 
-int main() {
-  int arr[] = {1, 2, 3, 6, 5, 4, 8, 7};
-  int n = 8;
+  bubbleSort(arr, n);
 
-  BubbleSort(arr, n);
-
+  for (int n : arr) {
+    cout << n << " ";
+  }
+  cout << endl;
   return 0;
 }
