f8c3ef0810c36cb5e4318094e86c0fc0c93133b4
shiva676466 <vsgvarikuppala.com>
2026-04-11 09:01:49 +0530

    updated

--- Searching/linearSearch.cpp
+++ Searching/linearSearch.cpp
@@ -1,21 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-int linearSearch(int arr[], int target, int n){
-    for(int i = 0; i < n; i++){
-        if(arr[i] == target){
-            return i;
-        }
-        else {
-            return -1;
-        }
+int linearSearch(int arr[], int target, int n) {
+  for (int i = 0; i < n; i++) {
+    if (arr[i] == target) {
+      return i;
     }
 }
+    return -1;
+}
 
-int main(void){
+int main(void) {
 
+  int arr[] = {1, 2, 6, 5, 4, 3};
+  int n = 6;
+  int target = 5;
 
+  cout << linearSearch(arr, target, n);
 
-    return 0;
+  return 0;
 }
