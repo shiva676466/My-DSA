a9a1b937f53f432be0f403ba6c015e01f11b0716
shiva676466 <vsgvarikuppala.com>
2026-04-14 19:12:26 +0530

    updated

--- Trees/BinaryTree.cpp
+++ Trees/BinaryTree.cpp
@@ -1,10 +1,44 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 
-struct BT_Node {
+class Node{
+public:
     int data;
-    struct BT_Node* left;
-    struct BT_Node* right;
+    Node* left;
+    Node* right;
+
+    // constructor for intialising variables everytime after creating an obj
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
 };
-;
+
+static int idx = -1;
+Node* buildTree(vector<int> preorder){
+    idx++;
+
+    if(preorder[idx] == -1){    // base case
+        return NULL;
+    }
+
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);   // for left
+    root->right = buildTree(preorder);   // for right
+
+    return root;
+}
+
+
+int main(){
+
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, 5, -1, -1};
+
+    Node* root = buildTree(preorder);
+
+    cout << root->data << "\n";
+
+    return 0;
+}
