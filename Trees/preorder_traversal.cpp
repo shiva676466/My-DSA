bc22ae927a6ae3e7e822a1efe2b8495aca4e2c97
shiva676466 <vsgvarikuppala.com>
2026-04-15 18:32:52 +0530

    update

--- Trees/preorder_traversal.cpp
+++ Trees/preorder_traversal.cpp
@@ -8,27 +8,36 @@ public:
   Node *left;
   Node *right;
 
+  // constructor for intialising variables everytime after creating an obj
   Node(int val) {
     data = val;
     left = right = NULL;
   }
 };
 
-Node *buildTree(vector<int> &preorder, int &idx) {
+Node *buildTree(vector<int>& preorder, int &idx) {
   idx++;
 
-  if (idx >= preorder.size() || preorder[idx] == -1) {
+  if (idx >= preorder.size() || preorder[idx] == -1) { // base case
     return NULL;
   }
 
   Node *root = new Node(preorder[idx]);
-
-  root->left = buildTree(preorder, idx);
-  root->right = buildTree(preorder, idx);
+  root->left = buildTree(preorder, idx);  // for left
+  root->right = buildTree(preorder, idx); // for right
 
   return root;
 }
 
-int main(void) {
-  int preorder = {} return 0;
+int main() {
+
+  vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, 5, -1, -1};
+  int idx = -1;
+  Node *root = buildTree(preorder, idx);
+
+  cout << root->data << " ";
+  cout << root->left->data << " ";
+  cout << root->right->data << " ";
+
+  return 0;
 }
