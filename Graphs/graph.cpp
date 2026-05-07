100732bcbd9bb97ad0fe2b6c0d9bb69422c15299
shiva676466 <vsgvarikuppala@gmail.com>
2026-05-07 23:45:50 +0530

    update

--- Graphs/graph.cpp
+++ Graphs/graph.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 
 /* 
+*================* THEORY *================*
+
 A graph is a data structure used to represnt the relationships between objects.
 - Objects: vertices(nodes)
 - Connections(relationship) between them called 'edges'
@@ -23,33 +25,71 @@ C ----- D
 Here, A, B, C, D are 'vertices' and the connected lines between them are called 'edges'.
 
 Types of graphs are based on different categories:
-1.Based on 'size':
+1.Based on 'SIZE':
     - finite graph: a finite graph is a graph with finite no of vertices and edges.
     - infinite graph: this graph has an infinite no of vertices and edges, these graphs nodes and edges are uncountable.
 
-2.Based on 'structure':
+2.Based on 'STRUCTURE':
     - trivial graph: a graph is said to be trivial if a finite graph having only 1 vertex and 0 edges.
-    - simple graph: this graph doesnt contain more than one edge b/w the pair of vertices. ex: (A->)
-    - multi graph
-    - null graph
-    - complete graph
+    - simple graph: this graph doesnt contain more than one edge b/w the pair of vertices.
+    - multi graph: graph which contains parallel edges but doesnt contains self-loop is a multi-graph
+    - null graph: a graph of 'order' and 'size' are 0 is called a null graph; order = no of vertices, size = no of edges
+    - complete graph: a simple graph of n vertices in which each node has (n-1) edges or 'degree' of each node is (n-1), means we can 
+                        travel to any vertex with the help of one vertex in this graph; degree of a vertex = no of edges connected to that vertex.
 
-3.Based on 'direction':
-    - directed graph
-    - un-directed graph
+3.Based on 'DIRECTION':
+    - directed graph: graph with edges having direction such that we cant able to travel directed side from the vertex to another one.
+    - un-directed graph: graph with edges having no direction so that we can travel to any vertex that is connected to the given vertex.
 
-4.Based on 'weight':
-    - weighted graph
-    - un-weighted graph
+4.Based on 'WEIGHT':
+    - weighted graph: graphs in which edges having weight(number) that represents cost, time or distance.
+    - un-weighted graph: graphs in which edges having no weight and all the edges are treated equally.
 
-5.Based on 'connectivity':
-    - connected graph
-    - disconnected graph
+5.Based on 'CONNECTIVITY':
+    - connected graph: a graph is said to be connected if there exists atleast one path b/w each n every pair of nodes (we can travel whole graph with the help of only one node).
+    - disconnected graph: a graph where all nodes are NOT reachable from each other is called a disconnected graph.
+*/
+
+/* 
+Graph Representation are of mainly 2 types:
+1. Adjacency Matrix: for the graph:      0 -- 1
+                                         |    |
+                                         2 -- 3
+    the adjacency matrix is: 
+                                      0   1   2   3
+                         rows:  0 : | 0 | 1 | 1 | 0
+                                1 : | 1 | 0 | 0 | 1
+                                2 : | 1 | 0 | 0 | 1
+                                3 : | 0 | 1 | 1 | 0
+2. Adjacency List
 */
 
 int main(void){
 
+    int n = 4;
+    
+    int adj[4][4] = {0};
 
+    // edges
+    adj[0][1] = 1;
+    adj[1][0] = 1;
+
+    adj[0][2] = 1;
+    adj[2][0] = 1;
+
+    adj[1][3] = 1;
+    adj[3][1] = 1;
+    
+    adj[2][3] = 1;
+    adj[3][2] = 1;
+
+    // print matrix
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << adj[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
