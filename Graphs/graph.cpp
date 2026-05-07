7ebbe84d867f1b1455e8aa2b03ca20d159a3461b
shiva676466 <vsgvarikuppala@gmail.com>
2026-05-07 23:11:21 +0530

    update

---
+++ Graphs/graph.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+
+/* 
+A graph is a data structure used to represnt the relationships between objects.
+- Objects: vertices(nodes)
+- Connections(relationship) between them called 'edges'
+Ex: 
+    - cities connected by roads
+    - users connected in social networks
+    - webpages connected by hyperlinks
+    - computers connected in networks
+
+Basic Terminology: 
+- consider this graph:
+
+A ----- B
+|       |
+|       |
+C ----- D
+Here, A, B, C, D are 'vertices' and the connected lines between them are called 'edges'.
+
+Types of graphs are based on different categories:
+1.Based on 'size':
+    - finite graph: a finite graph is a graph with finite no of vertices and edges.
+    - infinite graph: this graph has an infinite no of vertices and edges, these graphs nodes and edges are uncountable.
+
+2.Based on 'structure':
+    - trivial graph: a graph is said to be trivial if a finite graph having only 1 vertex and 0 edges.
+    - simple graph: this graph doesnt contain more than one edge b/w the pair of vertices. ex: (A->)
+    - multi graph
+    - null graph
+    - complete graph
+
+3.Based on 'direction':
+    - directed graph
+    - un-directed graph
+
+4.Based on 'weight':
+    - weighted graph
+    - un-weighted graph
+
+5.Based on 'connectivity':
+    - connected graph
+    - disconnected graph
+*/
+
+int main(void){
+
+
+
+    return 0;
+}
