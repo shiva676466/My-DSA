1bb112a99e834c780ae5781d012e77227fbab5fa
Shiva-Ganesh-Varikuppala <vsgvarikuppala@gmail.com>
2026-08-16 00:59:42 +0530

    update1

--- Patterns/PrefixSum/prefixSum.cpp
+++ Patterns/PrefixSum/prefixSum.cpp
@@ -11,7 +11,10 @@ int main(){
     int n = nums.size();
     vector<int> prefixSum(n + 1, 0);
 
-    
+    prefixSum[0] = nums[0];
+    for(int i = 1; i < n; i++){
+        prefixSum[i] = prefixSum[i] + nums[i];
+    }
 
     return 0;
 }
