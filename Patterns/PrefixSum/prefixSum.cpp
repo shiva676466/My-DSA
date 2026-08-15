1d04aa570a5d3a88022f40db27000a2a2f64b636
Shiva-Ganesh-Varikuppala <vsgvarikuppala@gmail.com>
2026-08-16 00:53:33 +0530

    update

---
+++ Patterns/PrefixSum/prefixSum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Build the prefix array using the n + 1 technique.
+// the given array is: [2, 5, 1, 8, 3]
+
+int main(){
+
+    vector<int> nums = {2, 5, 1, 8, 3};
+    int n = nums.size();
+    vector<int> prefixSum(n + 1, 0);
+
+    
+
+    return 0;
+}
